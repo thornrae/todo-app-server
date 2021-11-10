4d0af50e7567b5c68e778d76e8d095bfb8f34c9a
Taylor Thornton <thornrae@gmail.com>
2021-11-09 21:24:30 -0800

    try/catch

--- src/routes/tasks.js
+++ src/routes/tasks.js
@@ -10,43 +10,58 @@ const tasks = new MongoWrapper(tasksSchema);
 const tasksRouter = express.Router();
 
 tasksRouter.get('/tasks', getAllTasks);
-tasksRouter.get('/tasks/:id', getOneTask);
-tasksRouter.post('/tasks', createTask);
-tasksRouter.put('/tasks/:id', updateTask);
-tasksRouter.delete('/tasks/:id', deleteTask);
+// tasksRouter.get('/tasks/:id', getOneTask);
+// tasksRouter.post('/tasks', createTask);
+// tasksRouter.put('/tasks/:id', updateTask);
+// tasksRouter.delete('/tasks/:id', deleteTask);
 
 async function getAllTasks (req,res) {
-  let all = await tasks.get();
-  res.status(200).send(all);
+
+  try {
+    let all = await tasks.get();
+    res.status(200).send(all);
+  }
+  catch(err) {
+    console.log(err);
+  }
+  
 }
 
-async function getOneTask (req, res) {
-  let id = req.params.id;
-  let item = await tasks.get(id);
-  res.status(200).send(item);
-}
+// async function getAllTasks (req,res) {
+//   let all = await tasks.get();
+//   res.status(200).send(all);
+// }
 
-async function createTask (req, res) {
-  let obj = req.body;
-  console.log(obj);
-  let newTask = await tasks.create(obj);
-  console.log(newTask);
-  res.status(200).send(newTask);
-}
 
-async function updateTask (req, res) {
-  let id = req.params.id;
-  let update = req.body;
-  let updatedChange = await tasks.update(id, update);
-  res.status(200).send(updatedChange);
-}
 
-async function deleteTask (req, res){
-  let id = req.params.id;
-  console.log(id);
-  let done = await tasks.delete(id);
-  res.status(200).send(done);
-}
+
+// async function getOneTask (req, res) {
+//   let id = req.params.id;
+//   let item = await tasks.get(id);
+//   res.status(200).send(item);
+// }
+
+// async function createTask (req, res) {
+//   let obj = req.body;
+//   console.log(obj);
+//   let newTask = await tasks.create(obj);
+//   console.log(newTask);
+//   res.status(200).send(newTask);
+// }
+
+// async function updateTask (req, res) {
+//   let id = req.params.id;
+//   let update = req.body;
+//   let updatedChange = await tasks.update(id, update);
+//   res.status(200).send(updatedChange);
+// }
+
+// async function deleteTask (req, res){
+//   let id = req.params.id;
+//   console.log(id);
+//   let done = await tasks.delete(id);
+//   res.status(200).send(done);
+// }
 
 
 module.exports = tasksRouter;
